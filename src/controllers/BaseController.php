409c0d1fc35debf51d8d785ce698bce9d750b065
Josh Crawford <joshua@sgroup.com.au>
2019-07-25 16:41:40 +1000

    Add Navee migration + add panels to settings

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -3,6 +3,7 @@ namespace verbb\navigation\controllers;
 
 use verbb\navigation\Navigation;
 use verbb\navigation\migrations\AmNavPlugin;
+use verbb\navigation\migrations\NaveePlugin;
 
 use Craft;
 use craft\web\Controller;
@@ -12,7 +13,7 @@ class BaseController extends Controller
     // Public Methods
     // =========================================================================
 
-    public function actionMigrate()
+    public function actionAmNavMigrate()
     {
         // Backup!
         Craft::$app->getDb()->backup();
@@ -32,6 +33,31 @@ class BaseController extends Controller
         $output = nl2br($output);
 
         Craft::$app->getSession()->setNotice(Craft::t('navigation', 'A&M Nav migrated.'));
+
+        return $this->renderTemplate('navigation/settings', [
+            'output' => $output,
+            'settings' => $settings,
+        ]);
+    }
+
+    public function actionNaveeMigrate()
+    {
+        // Backup!
+        Craft::$app->getDb()->backup();
+
+        $settings = Navigation::$plugin->getSettings();
+        $request = Craft::$app->getRequest();
+
+        $migration = new NaveePlugin();
+
+        ob_start();
+        $migration->up();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $output = nl2br($output);
+
+        Craft::$app->getSession()->setNotice(Craft::t('navigation', 'Navee migrated.'));
 
         return $this->renderTemplate('navigation/settings', [
             'output' => $output,
---
+++ src/migrations/NaveePlugin.php
@@ -0,0 +1,134 @@
+<?php
+namespace verbb\navigation\migrations;
+
+use verbb\navigation\Navigation;
+use verbb\navigation\elements\Node;
+use verbb\navigation\models\Nav as NavModel;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+use craft\helpers\Json;
+
+class NaveePlugin extends Migration
+{
+    public $propagate = true;
+    public $assignToDefaultSite = false;
+
+    private $processedNodes = [];
+
+    // Public Methods
+    // =========================================================================
+
+    public function safeUp()
+    {
+        try {
+            if (!$this->db->tableExists('{{%navee_navigations}}')) {
+                return true;
+            }
+
+            $NaveeNavs = (new Query())
+                ->select(['*'])
+                ->from(['{{%navee_navigations}}'])
+                ->all();
+
+            foreach ($NaveeNavs as $key => $NaveeNav) {
+                $nav = Navigation::$plugin->navs->getNavByHandle($NaveeNav['handle']);
+
+                echo "\n    > Migrating nav `{$NaveeNav['handle']}` ...\n";
+
+                if (!$nav) {
+                    $nav = new NavModel();
+                }
+
+                $nav->name = $NaveeNav['name'];
+                $nav->handle = $NaveeNav['handle'];
+                $nav->maxLevels = $NaveeNav['maxLevels'];
+                $nav->structureId = $NaveeNav['structureId'];
+
+                if (!Navigation::$plugin->navs->saveNav($nav)) {
+                    echo "    > ERROR: Unable to migrate nav `{$NaveeNav['handle']}` ...\n";
+
+                    Craft::dump($nav->getErrors());
+
+                    continue;
+                }
+
+                foreach (Craft::$app->getSites()->getAllSites() as $site) {
+                    $NaveeNodes = (new Query())
+                        ->select(['*'])
+                        ->from(['{{%navee_nodes}}'])
+                        ->leftJoin('{{%elements_sites}} elements_sites', '[[elements_sites.elementId]] = [[navee_nodes.id]]')
+                        ->where(['navigationId' => $NaveeNav['id'], 'siteId' => $site['id']])
+                        ->all();
+
+                    foreach ($NaveeNodes as $key => $NaveeNode) {
+                        try {
+                            $node = new Node();
+
+                            $NaveeElement = (new Query())
+                                ->select(['*'])
+                                ->from(['{{%elements}}'])
+                                ->leftJoin('{{%content}} content', '[[content.elementId]] = [[elements.id]]')
+                                ->where(['elements.id' => $NaveeNode['elementId'], 'type' => 'Navee_Node', 'siteId' => $site['id']])
+                                ->one();
+
+                            if (!$NaveeElement) {
+                                echo "    > ERROR: Unable to find element for node `{$NaveeNode['id']}` ...\n";
+
+                                continue;
+                            }
+
+                            $node->title = $NaveeElement['title'];
+                            $node->enabled = $NaveeElement['enabled'];
+                            $node->siteId = $site->id;
+                            $node->navId = $nav->id;
+                            $node->url = $NaveeNode['customUri'];
+                            $node->classes = $NaveeNode['class'];
+
+                            if ($NaveeNode['target'] === '_blank') {
+                                $node->newWindow = true;
+                            }
+
+                            if ($NaveeNode['entryId']) {
+                               $node->type = \craft\elements\Entry::class;
+                               $node->elementId = $NaveeNode['entryId'];
+                            } else if ($NaveeNode['categoryId']) {
+                               $node->type = \craft\elements\Category::class;
+                               $node->elementId = $NaveeNode['categoryId'];
+                            } else if ($NaveeNode['assetId']) {
+                               $node->type = \craft\elements\Asset::class;
+                               $node->elementId = $NaveeNode['assetId'];
+                            }
+
+                            if (Craft::$app->getElements()->saveElement($node, true, $this->propagate)) {
+                                
+
+                            } else {
+                                echo "    > ERROR: Unable to save node `{$NaveeNode['title']}` ...\n";
+
+                                Craft::dump($node->getErrors());
+                            }
+                        } catch (\Throwable $e) {
+                            echo "    > ERROR: Unable to save node `{$NaveeNode['id']}` ...\n";
+
+                            Craft::dump($e->getMessage());
+
+                            continue;
+                        } 
+                    }
+                }
+            }
+
+        } catch (\Throwable $e) {
+            Craft::dump($e->getMessage());
+        }
+
+        return true;
+    }
+
+    public function safeDown()
+    {
+        return false;
+    }
+}
