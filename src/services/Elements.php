743b8f015dcb0ebd92c60a525ad667c202c04e29
Josh Crawford <joshua@sgroup.com.au>
2020-06-06 14:02:35 +1000

    Function cleanup

--- src/services/Elements.php
+++ src/services/Elements.php
@@ -50,7 +50,7 @@ class Elements extends Component
             ],
         ];
 
-        if ($this->isPluginInstalledAndEnabled('commerce')) {
+        if (Craft::$app->getPlugins()->isPluginEnabled('commerce')) {
             $elements['product'] = [
                 'label' => Craft::t('navigation', 'Products'),
                 'button' => Craft::t('navigation', 'Add a product'),
@@ -74,17 +74,6 @@ class Elements extends Component
         $this->trigger(self::EVENT_REGISTER_NAVIGATION_ELEMENT, $event);
 
         return $event->elements;
-    }
-
-    public function isPluginInstalledAndEnabled($pluginHandle)
-    {
-        $plugins = Craft::$app->getPlugins();
-
-        if ($plugins->isPluginInstalled($pluginHandle) && $plugins->isPluginEnabled($pluginHandle)) {
-            return true;
-        }
-
-        return false;
     }
 
 }
