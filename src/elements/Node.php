8d55af4bb1cae806ebfb3c156aa46d63f70a56cf
Josh Crawford <joshua@sgroup.com.au>
2021-01-19 20:37:56 +1100

    Fix passive nodes being marked as active

--- src/elements/Node.php
+++ src/elements/Node.php
@@ -692,7 +692,10 @@ class Node extends Element
 
             // If `$currentUrl` string equals `$nodeUrl` string, zero is returned - if this happens, a match is found.
             if (strpos($currentUrl, $nodeUrl) === 0) {
-                $isActive = true;
+                // Make sure we're not on the homepage (unless this node is for the homepage)
+                if ($nodeUrl !== $siteUrl) {
+                    $isActive = true;
+                }
             }
         }
 
