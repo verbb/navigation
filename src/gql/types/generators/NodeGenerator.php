5e96be91b3dd8865a395911de01c0baaa1c8a347
Josh Crawford <joshua@sgroup.com.au>
2021-10-21 23:58:01 +1100

    Fix GraphQL generator issues in some cases (Gatsby)

--- src/gql/types/generators/NodeGenerator.php
+++ src/gql/types/generators/NodeGenerator.php
@@ -9,13 +9,16 @@ use verbb\navigation\gql\types\NodeType;
 use verbb\navigation\helpers\Gql as NavigationGqlHelper;
 
 use Craft;
+use craft\gql\base\Generator;
 use craft\gql\base\GeneratorInterface;
+use craft\gql\base\ObjectType;
+use craft\gql\base\SingleGeneratorInterface;
 use craft\gql\GqlEntityRegistry;
 use craft\gql\TypeLoader;
 use craft\gql\TypeManager;
 use craft\helpers\Gql as GqlHelper;
 
-class NodeGenerator implements GeneratorInterface
+class NodeGenerator extends Generator implements GeneratorInterface, SingleGeneratorInterface
 {
     // Public Methods
     // =========================================================================
@@ -26,33 +29,38 @@ class NodeGenerator implements GeneratorInterface
         $gqlTypes = [];
 
         foreach ($navs as $nav) {
-            $typeName = Node::gqlTypeNameByContext($nav);
             $requiredContexts = Node::gqlScopesByContext($nav);
 
-            if (!NavigationGqlHelper::isSchemaAwareOf($requiredContexts)) {
+            if (!GqlHelper::isSchemaAwareOf($requiredContexts)) {
                 if (!GqlHelper::canSchema('navigationNavs.all')) {
                     continue;
                 }
             }
 
-            $contentFields = $nav->getFields();
-            $contentFieldGqlTypes = [];
-
-            foreach ($contentFields as $contentField) {
-                $contentFieldGqlTypes[$contentField->handle] = $contentField->getContentGqlType();
-            }
-
-            $navFields = TypeManager::prepareFieldDefinitions(array_merge(NodeInterface::getFieldDefinitions(), $contentFieldGqlTypes), $typeName);
-
             // Generate a type for each entry type
-            $gqlTypes[$typeName] = GqlEntityRegistry::getEntity($typeName) ?: GqlEntityRegistry::createEntity($typeName, new NodeType([
-                'name' => $typeName,
-                'fields' => function() use ($navFields) {
-                    return $navFields;
-                }
-            ]));
+            $type = static::generateType($entryType);
+            $gqlTypes[$type->name] = $type;
         }
 
         return $gqlTypes;
+    }
+
+    public static function generateType($context): ObjectType
+    {
+        $typeName = Node::gqlTypeNameByContext($context);
+
+        if ($createdType = GqlEntityRegistry::getEntity($typeName)) {
+            return $createdType;
+        }
+
+        $contentFieldGqlTypes = self::getContentFields($context);
+        $navFields = TypeManager::prepareFieldDefinitions(array_merge(NodeInterface::getFieldDefinitions(), $contentFieldGqlTypes), $typeName);
+
+        return GqlEntityRegistry::createEntity($typeName, new NodeType([
+            'name' => $typeName,
+            'fields' => function() use ($navFields) {
+                return $navFields;
+            },
+        ]));
     }
 }
