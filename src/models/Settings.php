ad421496bb2e185da87d23ab183c6e19fa83cc51
Josh Crawford <joshua@sgroup.com.au>
2026-09-09 20:08:20 +1000

    Normalize plugin settings.

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -23,4 +23,14 @@ class Settings extends Model
         parent::__construct($config);
     }
 
+    protected function defineRules(): array
+    {
+        $rules = parent::defineRules();
+        $rules[] = [['pluginName'], 'trim'];
+        $rules[] = [['pluginName'], 'required'];
+        $rules[] = [['pluginName'], 'string', 'max' => 52];
+
+        return $rules;
+    }
+
 }
